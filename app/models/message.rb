d1b71c6d94f7057d7dbf6b2688adae1de88ab8d3
syotatakei <takei.0618.syota@gmail.com>
2017-09-18 22:41:00 +0000

    new colum

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,5 @@ class Message < ApplicationRecord
     validates :title , length: { maximum: 32 } , presence: true
  validates :message , length: { maximum: 256 } , presence: true
  validates :name , length: { maximum: 32 }
+ validates :date , numericality: {greater_than:1}
 end
